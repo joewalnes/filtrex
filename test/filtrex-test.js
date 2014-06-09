1784f4543d87c0cdb79c50ada0f7270e5a1ddc7b
Alex Gorbatchev <alex.gorbatchev@gmail.com>
2014-06-09 07:02:55 -0700

    Updates tests to use mocha.

--- test/filtrex-test.js
+++ test/filtrex-test.js
@@ -1,128 +1,125 @@
+var expect = require('chai').expect;
 var compileExpression = require('../');
-var tt = require('./tinytest');
-var eq = tt.eq;
 
-tt.tests({
+describe('filtrex', function() {
+    it('simple numeric expressions', function() {
+        expect(compileExpression('1 + 2 * 3')()).to.equal(7);
+        expect(compileExpression('2 * 3 + 1')()).to.equal(7);
+        expect(compileExpression('1 + (2 * 3)')()).to.equal(7);
+        expect(compileExpression('(1 + 2) * 3')()).to.equal(9);
+        expect(compileExpression('((1 + 2) * 3 / 2 + 1 - 4 + (2 ^ 3)) * -2')()).to.equal(-19);
+        expect(compileExpression('1.4 * 1.1')()).to.equal(1.54);
+        expect(compileExpression('97 % 10')()).to.equal(7);
+    });
 
-    'simple numeric expressions': function() {
-        eq(7, compileExpression('1 + 2 * 3')());
-        eq(7, compileExpression('2 * 3 + 1')());
-        eq(7, compileExpression('1 + (2 * 3)')());
-        eq(9, compileExpression('(1 + 2) * 3')());
-        eq(-19, compileExpression('((1 + 2) * 3 / 2 + 1 - 4 + (2 ^ 3)) * -2')());
-        eq(1.54, compileExpression('1.4 * 1.1')());
-        eq(7, compileExpression('97 % 10')());
-    },
-
-    'bind to data': function() {
+    it('bind to data', function() {
         var something = compileExpression('1 + foo * bar');
-        eq(11, something({foo:5, bar:2})); 
-        eq(3, something({foo:2, bar:1})); 
-    },
+        expect(something({foo:5, bar:2})).to.equal(11);
+        expect(something({foo:2, bar:1})).to.equal(3);
+    });
 
-    'math functions': function() {
-        eq(5, compileExpression('abs(-5)')());
-        eq(5, compileExpression('abs(5)')());
-        eq(5, compileExpression('ceil(4.1)')());
-        eq(5, compileExpression('ceil(4.6)')());
-        eq(4, compileExpression('floor(4.1)')());
-        eq(4, compileExpression('floor(4.6)')());
-        eq(4, compileExpression('round(4.1)')());
-        eq(5, compileExpression('round(4.6)')());
-        eq(3, compileExpression('sqrt(9)')());
-    },
+    it('math functions', function() {
+        expect(compileExpression('abs(-5)')()).to.equal(5);
+        expect(compileExpression('abs(5)')()).to.equal(5);
+        expect(compileExpression('ceil(4.1)')()).to.equal(5);
+        expect(compileExpression('ceil(4.6)')()).to.equal(5);
+        expect(compileExpression('floor(4.1)')()).to.equal(4);
+        expect(compileExpression('floor(4.6)')()).to.equal(4);
+        expect(compileExpression('round(4.1)')()).to.equal(4);
+        expect(compileExpression('round(4.6)')()).to.equal(5);
+        expect(compileExpression('sqrt(9)')()).to.equal(3);
+    });
 
-    'functions with multiple args': function() {
-        eq(2, compileExpression('min(2)')());
-        eq(2, compileExpression('max(2)')());
-        eq(2, compileExpression('min(2, 5)')());
-        eq(5, compileExpression('max(2, 5)')());
-        eq(2, compileExpression('min(2, 5, 6)')());
-        eq(6, compileExpression('max(2, 5, 6)')());
-        eq(1, compileExpression('min(2, 5, 6, 1)')());
-        eq(6, compileExpression('max(2, 5, 6, 1)')());
-        eq(1, compileExpression('min(2, 5, 6, 1, 9)')());
-        eq(9, compileExpression('max(2, 5, 6, 1, 9)')());
-        eq(1, compileExpression('min(2, 5, 6, 1, 9, 12)')());
-        eq(12, compileExpression('max(2, 5, 6, 1, 9, 12)')());
-    },
+    it('functions with multiple args', function() {
+        expect(compileExpression('min(2)')()).to.equal(2);
+        expect(compileExpression('max(2)')()).to.equal(2);
+        expect(compileExpression('min(2, 5)')()).to.equal(2);
+        expect(compileExpression('max(2, 5)')()).to.equal(5);
+        expect(compileExpression('min(2, 5, 6)')()).to.equal(2);
+        expect(compileExpression('max(2, 5, 6)')()).to.equal(6);
+        expect(compileExpression('min(2, 5, 6, 1)')()).to.equal(1);
+        expect(compileExpression('max(2, 5, 6, 1)')()).to.equal(6);
+        expect(compileExpression('min(2, 5, 6, 1, 9)')()).to.equal(1);
+        expect(compileExpression('max(2, 5, 6, 1, 9)')()).to.equal(9);
+        expect(compileExpression('min(2, 5, 6, 1, 9, 12)')()).to.equal(1);
+        expect(compileExpression('max(2, 5, 6, 1, 9, 12)')()).to.equal(12);
+    });
 
-    'boolean logic': function() {
-        eq(0, compileExpression('0 and 0')());
-        eq(0, compileExpression('0 and 1')());
-        eq(0, compileExpression('1 and 0')());
-        eq(1, compileExpression('1 and 1')());
-        eq(0, compileExpression('0 or 0')());
-        eq(1, compileExpression('0 or 1')());
-        eq(1, compileExpression('1 or 0')());
-        eq(1, compileExpression('1 or 1')());
-        eq(1, compileExpression('not 0')());
-        eq(0, compileExpression('not 1')());
-        eq(1, compileExpression('(0 and 1) or 1')());
-        eq(0, compileExpression('0 and (1 or 1)')());
-        eq(1, compileExpression('0 and 1 or 1')()); // or is higher precedence
-        eq(1, compileExpression('1 or 1 and 0')()); // or is higher precedence
-        eq(0, compileExpression('not 1 and 0')()); // not is higher precedence
-    },
+    it('boolean logic', function() {
+        expect(compileExpression('0 and 0')()).to.equal(0);
+        expect(compileExpression('0 and 1')()).to.equal(0);
+        expect(compileExpression('1 and 0')()).to.equal(0);
+        expect(compileExpression('1 and 1')()).to.equal(1);
+        expect(compileExpression('0 or 0')()).to.equal(0);
+        expect(compileExpression('0 or 1')()).to.equal(1);
+        expect(compileExpression('1 or 0')()).to.equal(1);
+        expect(compileExpression('1 or 1')()).to.equal(1);
+        expect(compileExpression('not 0')()).to.equal(1);
+        expect(compileExpression('not 1')()).to.equal(0);
+        expect(compileExpression('(0 and 1) or 1')()).to.equal(1);
+        expect(compileExpression('0 and (1 or 1)')()).to.equal(0);
+        expect(compileExpression('0 and 1 or 1')()).to.equal(1); // or is higher precedence
+        expect(compileExpression('1 or 1 and 0')()).to.equal(1); // or is higher precedence
+        expect(compileExpression('not 1 and 0')()).to.equal(0); // not is higher precedence
+    });
 
-    'comparisons': function() {
-        eq(1, compileExpression('foo == 4')({foo:4}));
-        eq(0, compileExpression('foo == 4')({foo:3}));
-        eq(0, compileExpression('foo == 4')({foo:-4}));
-        eq(0, compileExpression('foo != 4')({foo:4}));
-        eq(1, compileExpression('foo != 4')({foo:3}));
-        eq(1, compileExpression('foo != 4')({foo:-4}));
-        eq(0, compileExpression('foo > 4')({foo:3}));
-        eq(0, compileExpression('foo > 4')({foo:4}));
-        eq(1, compileExpression('foo > 4')({foo:5}));
-        eq(0, compileExpression('foo >= 4')({foo:3}));
-        eq(1, compileExpression('foo >= 4')({foo:4}));
-        eq(1, compileExpression('foo >= 4')({foo:5}));
-        eq(1, compileExpression('foo < 4')({foo:3}));
-        eq(0, compileExpression('foo < 4')({foo:4}));
-        eq(0, compileExpression('foo < 4')({foo:5}));
-        eq(1, compileExpression('foo <= 4')({foo:3}));
-        eq(1, compileExpression('foo <= 4')({foo:4}));
-        eq(0, compileExpression('foo <= 4')({foo:5}));
-    },
+    it('comparisons', function() {
+        expect(compileExpression('foo == 4')({foo:4})).to.equal(1);
+        expect(compileExpression('foo == 4')({foo:3})).to.equal(0);
+        expect(compileExpression('foo == 4')({foo:-4})).to.equal(0);
+        expect(compileExpression('foo != 4')({foo:4})).to.equal(0);
+        expect(compileExpression('foo != 4')({foo:3})).to.equal(1);
+        expect(compileExpression('foo != 4')({foo:-4})).to.equal(1);
+        expect(compileExpression('foo > 4')({foo:3})).to.equal(0);
+        expect(compileExpression('foo > 4')({foo:4})).to.equal(0);
+        expect(compileExpression('foo > 4')({foo:5})).to.equal(1);
+        expect(compileExpression('foo >= 4')({foo:3})).to.equal(0);
+        expect(compileExpression('foo >= 4')({foo:4})).to.equal(1);
+        expect(compileExpression('foo >= 4')({foo:5})).to.equal(1);
+        expect(compileExpression('foo < 4')({foo:3})).to.equal(1);
+        expect(compileExpression('foo < 4')({foo:4})).to.equal(0);
+        expect(compileExpression('foo < 4')({foo:5})).to.equal(0);
+        expect(compileExpression('foo <= 4')({foo:3})).to.equal(1);
+        expect(compileExpression('foo <= 4')({foo:4})).to.equal(1);
+        expect(compileExpression('foo <= 4')({foo:5})).to.equal(0);
+    });
 
-    'in / not in': function() {
-        eq(0, compileExpression('5 in (1, 2, 3, 4)')());
-        eq(1, compileExpression('3 in (1, 2, 3, 4)')());
-        eq(1, compileExpression('5 not in (1, 2, 3, 4)')());
-        eq(0, compileExpression('3 not in (1, 2, 3, 4)')());
-    },
+    it('in / not in', function() {
+        expect(compileExpression('5 in (1, 2, 3, 4)')()).to.equal(false);
+        expect(compileExpression('3 in (1, 2, 3, 4)')()).to.equal(true);
+        expect(compileExpression('5 not in (1, 2, 3, 4)')()).to.equal(true);
+        expect(compileExpression('3 not in (1, 2, 3, 4)')()).to.equal(false);
+    });
 
-    'string test': function() {
-        eq(1, compileExpression('foo == "hello"')({foo:'hello'}));
-        eq(0, compileExpression('foo == "hello"')({foo:'bye'}));
-        eq(0, compileExpression('foo != "hello"')({foo:'hello'}));
-        eq(1, compileExpression('foo != "hello"')({foo:'bye'}));
-        eq(1, compileExpression('foo in ("aa", "bb")')({foo:'aa'}));
-        eq(0, compileExpression('foo in ("aa", "bb")')({foo:'c'}));
-        eq(0, compileExpression('foo not in ("aa", "bb")')({foo:'aa'}));
-        eq(1, compileExpression('foo not in ("aa", "bb")')({foo:'cc'}));
-    },
+    it('string test', function() {
+        expect(compileExpression('foo == "hello"')({foo:'hello'})).to.equal(1);
+        expect(compileExpression('foo == "hello"')({foo:'bye'})).to.equal(0);
+        expect(compileExpression('foo != "hello"')({foo:'hello'})).to.equal(0);
+        expect(compileExpression('foo != "hello"')({foo:'bye'})).to.equal(1);
+        expect(compileExpression('foo in ("aa", "bb")')({foo:'aa'})).to.equal(true);
+        expect(compileExpression('foo in ("aa", "bb")')({foo:'c'})).to.equal(false);
+        expect(compileExpression('foo not in ("aa", "bb")')({foo:'aa'})).to.equal(false);
+        expect(compileExpression('foo not in ("aa", "bb")')({foo:'cc'})).to.equal(true);
+    });
 
-    'a ? b : c': function() {
-        eq(4, compileExpression('1 > 2 ? 3 : 4')());
-        eq(3, compileExpression('1 < 2 ? 3 : 4')());
-    },
+    it('a ? b : c', function() {
+        expect(compileExpression('1 > 2 ? 3 : 4')()).to.equal(4);
+        expect(compileExpression('1 < 2 ? 3 : 4')()).to.equal(3);
+    });
 
-    'kitchensink': function() {
+    it('kitchensink', function() {
         var kitchenSink = compileExpression('4 > lowNumber * 2 and (max(a, b) < 20 or foo) ? 1.1 : 9.4');
-        eq(1.1, kitchenSink({lowNumber:1.5, a:10, b:12, foo:false}));
-        eq(9.4, kitchenSink({lowNumber:3.5, a:10, b:12, foo:false}));
-    },
+        expect(kitchenSink({lowNumber:1.5, a:10, b:12, foo:false})).to.equal(1.1);
+        expect(kitchenSink({lowNumber:3.5, a:10, b:12, foo:false})).to.equal(9.4);
+    });
 
-    'symbols with dots': function() {
-        eq(123, compileExpression('hello.world.foo')({'hello.world.foo': 123}));
-        eq(123, compileExpression('order.gooandstuff')({'order.gooandstuff': 123}));
-    },
+    it('symbols with dots', function() {
+        expect(compileExpression('hello.world.foo')({'hello.world.foo': 123})).to.equal(123);
+        expect(compileExpression('order.gooandstuff')({'order.gooandstuff': 123})).to.equal(123);
+    });
 
-    'custom functions': function() {
+    it('custom functions', function() {
         function triple(x) { return x * 3; };
-        eq(21, compileExpression('triple(v)', {triple:triple})({v:7}));
-    },
-
+        expect(compileExpression('triple(v)', {triple:triple})({v:7})).to.equal(21);
+    });
 });
